b1a505279b724a74b5991c37f3622f0c8313c588
Avenon <silaevegor@hotmail.com>
2021-04-15 20:10:04 +0300

    feat: add env config

--- src/main.py
+++ src/main.py
@@ -3,6 +3,17 @@ import time
 import psutil
 import schedule
 
+from dotenv import find_dotenv, load_dotenv
+from envparse import env
+
+
+load_dotenv(find_dotenv(".env"))
+
+SPACE_LIMIT_WARNING: int = env.int("SPACE_LIMIT_WARNING")
+SPACE_LIMIT_ERROR: int = env.int("SPACE_LIMIT_ERROR")
+CHECK_PERIOD: int = env.int("CHECK_PERIOD")
+MOUNT_PATH: str = env.str("MOUNT_PATH")
+
 
 def send_alarm_warning(space_left):
     print(f"WARNING: Only {space_left:.2f} GB left!")
@@ -13,17 +24,17 @@ def send_alarm_error(space_left):
 
 
 def check_hdd():
-    hdd = psutil.disk_usage("/")
+    hdd = psutil.disk_usage(MOUNT_PATH)
     free_gb = hdd.free / (1024 * 1024 * 1024)
-    if (free_gb > 2) and (free_gb <= 3):
+    if (free_gb > SPACE_LIMIT_ERROR) and (free_gb <= SPACE_LIMIT_WARNING):
         send_alarm_warning(free_gb)
-    elif free_gb <= 2:
+    elif free_gb <= SPACE_LIMIT_ERROR:
         send_alarm_error(free_gb)
     else:
         print(f"{free_gb:.2f} GB are free")
 
 
-schedule.every(5).seconds.do(check_hdd)
+schedule.every(CHECK_PERIOD).seconds.do(check_hdd)
 
 
 while True:
